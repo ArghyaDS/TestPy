e39c6b58f0044b0d92952039ebfdd5686f7f51cc
ArghyaDS <50789699+ArghyaDS@users.noreply.github.com>
2019-05-19 11:27:44 +0530

    Add files via upload

---
+++ AssertPy.py
@@ -0,0 +1,8 @@
+div=int(input("Enter dividend: "))
+divs=int(input("Enter divisor: "))
+try:
+    assert divs!=0, "You can not divide by zero"
+    quo=div/divs
+    print(f"Result: {quo}")
+except:
+    print("You should not try to divide by zero")
